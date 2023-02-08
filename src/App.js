949ffc85721df44313bbada266376265bac1198c
dattajadhav7263@gmail.com <95628448+lara-stack@users.noreply.github.com>
2023-02-08 15:10:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import './App.css';
-import ReactDOM from 'react-dom';
-import GoogleLogin from 'react-google-login';
+import React, { useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
 
-function App() {
+function App() {  
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: '176788460466-n9i9tp8jqfu79bjeprvbirfhqsrt90e5.apps.googleusercontent.com',
+        scope: 'email',
+      });
+    }
 
-  const responseGoogle = (response) => {
-    console.log(response);
-  }
+    gapi.load('client:auth2', start);
+  }, []);
+
+
+  // **you can access the token like this**
+  // const accessToken = gapi.auth.getToken().access_token;
+  // console.log(accessToken);
+
+  const onSuccess = response => {
+    console.log('SUCCESS', response);
+  };
+  const onFailure = response => {
+    console.log('FAILED', response);
+  };
+  const onLogoutSuccess = () => {
+    console.log('SUCESS LOG OUT');
+  };
 
   return (
-    <div className="App">
-     <GoogleLogin
-        clientId="176788460466-n9i9tp8jqfu79bjeprvbirfhqsrt90e5.apps.googleusercontent.com"
-        buttonText="Login with Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
+    <div>
+      <GoogleLogin
+        clientId='176788460466-n9i9tp8jqfu79bjeprvbirfhqsrt90e5.apps.googleusercontent.com'
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+      />
+      <GoogleLogout
+        clientId='176788460466-n9i9tp8jqfu79bjeprvbirfhqsrt90e5.apps.googleusercontent.com'
+        onLogoutSuccess={onLogoutSuccess}
       />
     </div>
   );
